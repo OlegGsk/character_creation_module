d7336f57aec6427db51dc88295bd05062c4d7e99
Олег Говоровский <oleq.gov@ya.ru>
2023-08-23 22:00:49 +0300

    add docstring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-'''Программа для создания персонажа.
+"""Программа для создания персонажа.
 Функции для выбора класса персонажа,для тренировки ,
 для расчета урона и блокирования.
-'''
+"""
 from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
